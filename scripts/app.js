aa27dcde5abbfa11d2769e4b7b16556fc00a0d4b
Alison Kenney <alisonkenney1@gmail.com>
2016-06-22 15:16:57 -0600

    geoquakes parts 1-4

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,26 @@ var map;
 var template;
 
 $(document).on("ready", function() {
+	var source = $("#quake-template").html();
+	var template = Handlebars.compile(source);
 
+	$.get(weekly_quakes_endpoint, function(data) {
+		console.log(data);
+		var quakeResults = data.features;
+		console.log(quakeResults);
 
+		var trackHtml = template({quakes: quakeResults});
+		$("#info").append(trackHtml);
+
+		quakeResults.forEach(function(currQuake) {
+			var lat = currQuake.geometry.coordinates[0];
+			var long = currQuake.geometry.coordinates[1];
+			var latLong = {lat: lat, lng: long};
+			var marker = new google.maps.Marker({
+				position: latLong,
+				map: map,
+				title: currQuake.properties.title,
+			});
+		});
+	});
 });
